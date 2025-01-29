597f9794bffc0962d76266dd511d443a79b7a22a
Ilyas023 <ilias_03@bk.ru>
2025-01-29 18:46:32 +0500

    feature/add-migration

--- WebOrderProject.Application/DTOs/ProductDto.cs
+++ WebOrderProject.Application/DTOs/ProductDto.cs
@@ -12,12 +12,9 @@ namespace WebOrderProject.Application.DTOs;
 
 public class ProductDto
 {
-    [SwaggerSchema(ReadOnly = true, Description = "Unique identifier of the product")]
     public Guid ProductId { get; set; }
     public string Name { get; set; } = string.Empty;
     public decimal Price { get; set; }
     public int Quantity { get; set; }
-    [SwaggerSchema(ReadOnly = true, Description = "Date and time when the product was created")]
-    [JsonIgnore]
     public DateTime CreatedAt { get; set; }
 }
